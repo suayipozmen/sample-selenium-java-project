ee350053b9843dda4edca3322a47ee013b6b9958
suayipozmen <suayipozmen@gmail.com>
2021-04-15 14:32:41 +0300

    removed www from url.

--- src/test/java/com/sahabt/base/BaseTest.java
+++ src/test/java/com/sahabt/base/BaseTest.java
@@ -52,7 +52,7 @@ public class BaseTest {
 
     @Test
     public void testCase3() {
-        navigate("https://www.dashboard.qa/");
+        navigate("https://dashboard.qa/");
     }
 
     public void navigate(String url) {
